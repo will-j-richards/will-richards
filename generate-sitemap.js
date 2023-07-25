c991fea01091b46fbd178b12adb50a777dde4a2f
will-j-richards <140435419+will-j-richards@users.noreply.github.com>
2023-07-25 11:49:54 +0100

    Updated sitemap

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,7 +1,7 @@
 const SitemapGenerator = require('sitemap-generator');
 const path = require('path');
 
-const generator = SitemapGenerator('https://will-richards.netlify.app/', {
+const generator = SitemapGenerator('https://http://willrichards.me/', {
   stripQuerystring: false, // Set this to true if you want to strip query parameters from URLs in the sitemap.
   filepath: path.join(__dirname, 'sitemap.xml'), // Output file path for the sitemap.
 });
